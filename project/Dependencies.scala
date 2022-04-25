0e8d30149addf2e952350acc279fe5a3573141b8
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-25 08:22:10 +0000

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.1"
   private val elasticMqVersion = "1.3.7"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
