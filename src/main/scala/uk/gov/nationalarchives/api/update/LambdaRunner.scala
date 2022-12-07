b7ba805ff7eab42b57706d3ace5ec0cef1a31c62
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-12-07 12:09:13 +0000

    Update the LambdRunner json

--- src/main/scala/uk/gov/nationalarchives/api/update/LambdaRunner.scala
+++ src/main/scala/uk/gov/nationalarchives/api/update/LambdaRunner.scala
@@ -10,41 +10,45 @@ object LambdaRunner extends App {
       |    {
       |      "fileId": "20d80488-d247-47cf-8687-be26de2558b5",
       |      "originalPath": "smallfile/subfolder/subfolder-nested/subfolder-nested-1.txt",
+      |      "fileSize": "2",
+      |      "clientChecksum": "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7",
       |      "consignmentId": "cedba409-c806-439f-8982-943afb03c85a",
       |      "userId": "030cf12c-8d5d-46b9-b86a-38e0920d0e1a",
-      |      "results": [
-      |        {
-      |          "antivirus": {
+      |      "fileCheckResults": {
+      |        "antivirus": [
+      |          {
       |            "software": "yara",
       |            "softwareVersion": "4.2.0",
       |            "databaseVersion": "$LATEST",
       |            "result": "",
-      |            "datetime": 1670335345994,
+      |            "datetime": 1670411830303,
       |            "fileId": "20d80488-d247-47cf-8687-be26de2558b5"
       |          }
-      |        },
-      |        {
-      |          "fileId": "20d80488-d247-47cf-8687-be26de2558b5",
-      |          "software": "Droid",
-      |          "softwareVersion": "6.6.0-rc2",
-      |          "binarySignatureFileVersion": "109",
-      |          "containerSignatureFileVersion": "20221102",
-      |          "method": "pronom",
-      |          "matches": [
-      |            {
-      |              "extension": "txt",
-      |              "identificationBasis": "Extension",
-      |              "puid": "x-fmt/111"
-      |            }
-      |          ]
-      |        },
-      |        {
-      |          "checksum": {
+      |        ],
+      |        "checksum": [
+      |          {
       |            "fileId": "20d80488-d247-47cf-8687-be26de2558b5",
       |            "sha256Checksum": "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7"
       |          }
-      |        }
-      |      ]
+      |        ],
+      |        "fileFormat": [
+      |          {
+      |            "fileId": "20d80488-d247-47cf-8687-be26de2558b5",
+      |            "software": "Droid",
+      |            "softwareVersion": "6.6.0-rc2",
+      |            "binarySignatureFileVersion": "109",
+      |            "containerSignatureFileVersion": "20221102",
+      |            "method": "pronom",
+      |            "matches": [
+      |              {
+      |                "extension": "txt",
+      |                "identificationBasis": "Extension",
+      |                "puid": "x-fmt/111"
+      |              }
+      |            ]
+      |          }
+      |        ]
+      |      }
       |    }
       |  ],
       |  "redactedResults": {
