7ab958bb4ee7949053451427f35e07cffc543dc5
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-10-16 08:22:27 +0000

    Update tdr-auth-utils to 0.0.171

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,5 +20,5 @@ object Dependencies {
   lazy val keycloakMock = "com.tngtech.keycloakmock" % "mock" % "0.16.0"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.27"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.2"
-  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.170"
+  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.171"
 }
