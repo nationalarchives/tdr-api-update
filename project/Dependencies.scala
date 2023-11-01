079d1a27f04ee19e17b5f296a0b549fda1abfcea
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-01 08:22:20 +0000

    Update tdr-backend-checks-utils to 0.1.34

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   private val awsUtilsVersion = "0.1.65"
 
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.21.11"
-  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.32"
+  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.34"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
